168136d8197315bb2f81f9a6d9d528b1f723536c
mu001999 <mu001999@outlook.com>
2018-12-30 22:23:54 +0800

    update PUEF.hpp

--- include/PUEF.hpp
+++ include/PUEF.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
 
+#include <queue>
+#include <mutex>
+#include <memory>
 #include <string>
 #include <sstream>
 #include <utility>
 #include <algorithm>
 #include <unordered_map>
+#include <condition_variable>
+
 
 #include <cstring>
 
@@ -14,6 +19,7 @@
 #include <sys/socket.h>
 
 
+#include "threadsafe_queue.hpp"
 #include "request.hpp"
 #include "spider.hpp"
 #include "parser.hpp"
