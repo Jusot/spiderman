c984a93f6bf809c5d986094aa8b7926e1ccafa32
mu001999 <mu001999@outlook.com>
2019-01-05 11:04:01 +0800

    update results' structure

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-    threadsafe_queue<::std::string> results;
+    threadsafe_queue<::std::pair<::std::string, ::std::string>> results;
 
     Spider spdr("www.gmw.cn", "www.gmw.cn");
 
     ::std::vector<::std::thread> threads;
 
     // main thread
-    threads.emplace_back([&] {spdr.run(results); });
+    threads.emplace_back([&] { spdr.run(results); });
 
     // sub thread
     for (int i = 0; i < 5; ++i) threads.emplace_back([&] 
     {
-        ::std::string result;
+        ::std::pair<::std::string, ::std::string> result;
 
         while (!spdr.finish() || !results.is_empty())
         {
